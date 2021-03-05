3b39df48efc0642c7c480ca7688efb861b4d2b4a
marnyansky <marnyansky@gmail.com>
2021-03-05 11:07:38 +0200

    sum of squared elements of list

---
+++ unit312239 step2.py
@@ -0,0 +1,6 @@
+"""
+url: https://stepik.org/lesson/328948/step/2?unit=312239
+Сумма квадратов элементов списка
+"""
+numbers = [1, 78, 23, -65, 99, 9089, 34, -32, 0, -67, 1, 11, 111]
+print(sum([i**2 for i in numbers]))
