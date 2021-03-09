797bc0b42cbeeda871b62e7a8c0066ac2e78cf4a
marnyansky <marnyansky@gmail.com>
2021-03-09 20:09:58 +0200

    str of numbers multi-validation

--- unit317559 step8.py
+++ unit317559 step8.py
@@ -6,14 +6,13 @@ example: 1221:101:22
 
 # my solution:
 def is_valid_password(p):
+    is_palindrome = False
     nums = [int(i) for i in p.split(':')]
     if str(nums[0]) == str(nums[0])[::-1] and len(nums) == 3:
         is_palindrome = True
-    else:
-        is_palindrome = False
 
     primes = 1
-    for i in range(2, nums[1] + 1):
+    for i in range(2, nums[1]+1):
         if nums[1] % i == 0:
             primes += 1
     is_prime = primes == 2
@@ -25,4 +24,6 @@ def is_valid_password(p):
 # alternative solution 1:
 def is_valid_password(password):
     password = password.split(':')
-    return (password[0] == password[0][::-1]) and (len([i for i in range(1, int(password[1])+1) if int(password[1]) % i == 0]) == 2) and (int(password[2]) % 2 == 0)
+    return (password[0] == password[0][::-1])
+            and (len([i for i in range(1, int(password[1])+1) if int(password[1]) % i == 0]) == 2)
+            and (int(password[2]) % 2 == 0)
