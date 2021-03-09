260d91891e14ad265c7de393b7a862809bd9f879
marnyansky <marnyansky@gmail.com>
2021-03-09 17:11:06 +0200

    str of numbers multi-validation

---
+++ unit317559 step8.py
@@ -0,0 +1,28 @@
+"""
+URL: https://stepik.org/lesson/334150/step/8?unit=317559
+boolean for a:b:c string (a: palindrome, b: prime, c: even)
+example: 1221:101:22
+"""
+
+# my solution:
+def is_valid_password(p):
+    nums = [int(i) for i in p.split(':')]
+    if str(nums[0]) == str(nums[0])[::-1] and len(nums) == 3:
+        is_palindrome = True
+    else:
+        is_palindrome = False
+
+    primes = 1
+    for i in range(2, nums[1] + 1):
+        if nums[1] % i == 0:
+            primes += 1
+    is_prime = primes == 2
+
+    is_even = nums[2] % 2 == 0
+
+    return is_palindrome and is_prime and is_even
+
+# alternative solution 1:
+def is_valid_password(password):
+    password = password.split(':')
+    return (password[0] == password[0][::-1]) and (len([i for i in range(1, int(password[1])+1) if int(password[1]) % i == 0]) == 2) and (int(password[2]) % 2 == 0)
